274b42eb445c278e07420cc9601b3736eb3a092a
Jean-Guilhem Rouel <jean-gui@w3.org>
2014-07-14 19:32:30 +0200

    Added bard and paladin classes

--- src/Troulite/PathfinderBundle/DataFixtures/ORM/LoadClassDefinitionData.php
+++ src/Troulite/PathfinderBundle/DataFixtures/ORM/LoadClassDefinitionData.php
@@ -97,6 +97,111 @@ class LoadClassDefinitionData extends AbstractFixture implements OrderedFixtureI
         $manager->flush();
 
         $this->addReference('barbarian', $barbarian);
+
+        $bab       = array();
+        $reflexes  = array();
+        $fortitude = array();
+        $will      = array();
+        for ($i = 0; $i < 20; $i++) {
+            $bab[]       = $i + 1;
+            $reflexes[]  = ((int)(($i + 1) / 3)) ;
+            $fortitude[] = ((int)(($i + 1) / 2)) + 2;
+            $will[]      = (int)(($i + 1) / 2) + 2;
+        }
+        $spellsPerDay = array(
+            1 => array(-1, -1, -1, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4),
+            2 => array(-1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4),
+            3 => array(-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3),
+            4 => array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 2, 2, 3)
+        );
+
+        $paladin = new ClassDefinition();
+        $paladin
+            ->setName("Paladin")
+            ->setHpDice(10)
+            ->setSkillPoints(2)
+            ->setBab($bab)
+            ->setReflexes($reflexes)
+            ->setFortitude($fortitude)
+            ->setWill($will)
+            ->setSpellsPerDay($spellsPerDay)
+            ->addClassSkill($this->getReference('craft'))
+            ->addClassSkill($this->getReference('diplomacy'))
+            ->addClassSkill($this->getReference('handleAnimal'))
+            ->addClassSkill($this->getReference('heal'))
+            ->addClassSkill($this->getReference('knowledgeNobility'))
+            ->addClassSkill($this->getReference('knowledgeReligion'))
+            ->addClassSkill($this->getReference('profession'))
+            ->addClassSkill($this->getReference('ride'))
+            ->addClassSkill($this->getReference('senseMotive'))
+            ->addClassSkill($this->getReference('spellcraft'));
+
+        $manager->persist($paladin);
+        $manager->flush();
+
+        $this->addReference('paladin', $paladin);
+
+        $bab       = array(0,1,2,3,3,4,5,6,6,7,8,9,9,10,11,12,12,13,14,15);
+        $reflexes  = array();
+        $fortitude = array();
+        $will      = array();
+        for ($i = 0; $i < 20; $i++) {
+            $reflexes[]  = ((int)(($i + 1) / 2)) + 2;
+            $fortitude[] = ((int)(($i + 1) / 3));
+            $will[]      = (int)(($i + 1) / 2) + 2;
+        }
+        $spellsPerDay = array(
+            1 => array( 1,  2,  3,  3,  4,  4,  4,  4,  5,  5,  5,  5,  5,  5,  5, 5, 5, 5, 5, 5),
+            2 => array(-1, -1, -1,  1,  2,  3,  3,  4,  4,  4,  4,  5,  5,  5,  5, 5, 5, 5, 5, 5),
+            3 => array(-1, -1, -1, -1, -1, -1,  1,  2,  3,  3,  4,  4,  4,  4,  5, 5, 5, 5, 5, 5),
+            4 => array(-1, -1, -1, -1, -1, -1, -1, -1, -1,  1,  2,  3,  3,  4,  4, 4, 4, 5, 5, 5),
+            5 => array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  1,  2,  3, 3, 4, 4, 5, 5),
+            6 => array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 2, 3, 4, 5),
+        );
+
+        $bard = new ClassDefinition();
+        $bard
+            ->setName("Bard")
+            ->setHpDice(8)
+            ->setSkillPoints(6)
+            ->setBab($bab)
+            ->setReflexes($reflexes)
+            ->setFortitude($fortitude)
+            ->setWill($will)
+            ->setSpellsPerDay($spellsPerDay)
+            ->addClassSkill($this->getReference('acrobatics'))
+            ->addClassSkill($this->getReference('appraise'))
+            ->addClassSkill($this->getReference('bluff'))
+            ->addClassSkill($this->getReference('climb'))
+            ->addClassSkill($this->getReference('craft'))
+            ->addClassSkill($this->getReference('diplomacy'))
+            ->addClassSkill($this->getReference('disguise'))
+            ->addClassSkill($this->getReference('escapeArtist'))
+            ->addClassSkill($this->getReference('intimidate'))
+            ->addClassSkill($this->getReference('knowledgeArcana'))
+            ->addClassSkill($this->getReference('knowledgeDungeoneering'))
+            ->addClassSkill($this->getReference('knowledgeEngineering'))
+            ->addClassSkill($this->getReference('knowledgeGeography'))
+            ->addClassSkill($this->getReference('knowledgeHistory'))
+            ->addClassSkill($this->getReference('knowledgeLocal'))
+            ->addClassSkill($this->getReference('knowledgeNature'))
+            ->addClassSkill($this->getReference('knowledgeNobility'))
+            ->addClassSkill($this->getReference('knowledgePlanes'))
+            ->addClassSkill($this->getReference('knowledgeReligion'))
+            ->addClassSkill($this->getReference('linguistics'))
+            ->addClassSkill($this->getReference('perception'))
+            ->addClassSkill($this->getReference('perform'))
+            ->addClassSkill($this->getReference('profession'))
+            ->addClassSkill($this->getReference('senseMotive'))
+            ->addClassSkill($this->getReference('sleightOfHand'))
+            ->addClassSkill($this->getReference('spellcraft'))
+            ->addClassSkill($this->getReference('stealth'))
+            ->addClassSkill($this->getReference('useMagicDevice'));
+
+        $manager->persist($bard);
+        $manager->flush();
+
+        $this->addReference('bard',$bard);
     }
 
     /**
