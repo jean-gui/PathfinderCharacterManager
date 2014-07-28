c714bfaf1fe68ed583df24807c750a856f7f35ef
Jean-Guilhem Rouel <jean-gui@w3.org>
2014-07-28 02:25:17 +0200

    Popovers actually have an option for automatic placement

--- src/Troulite/PathfinderBundle/Resources/public/popover.js
+++ src/Troulite/PathfinderBundle/Resources/public/popover.js
@@ -4,17 +4,7 @@
 
 $(document).ready(function () {
     $('.mypopover').popover({
-        placement: function (tip, element) {
-            var offset = $(element).offset();
-            height = $(document).outerHeight();
-            width = $(document).outerWidth();
-            vert = 0.5 * height - offset.top;
-            vertPlacement = vert > 0 ? 'bottom' : 'top';
-            horiz = 0.5 * width - offset.left;
-            horizPlacement = horiz > 0 ? 'right' : 'left';
-            placement = Math.abs(horiz) > Math.abs(vert) ? horizPlacement : vertPlacement;
-            return placement;
-        },
+        placement: 'auto',
         html: true,
         container: 'body'
     });
