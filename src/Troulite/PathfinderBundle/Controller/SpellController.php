88771294c54eaca6d741039559aa07c67d506e76
Jean-Guilhem Rouel <jean-gui@w3.org>
2014-07-29 21:54:33 +0200

    Added a basic controller for spells

---
+++ src/Troulite/PathfinderBundle/Controller/SpellController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Troulite\PathfinderBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Troulite\PathfinderBundle\Entity\Spell;
+
+/**
+ * Spell controller.
+ *
+ * @Route("/spells")
+ */
+class SpellController extends Controller
+{
+
+    /**
+     * Lists all Spell entities.
+     *
+     * @Route("/", name="spells")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('TroulitePathfinderBundle:Spell')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Spell entity.
+     *
+     * @Route("/{id}", name="spells_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction(Spell $entity)
+    {
+        return array(
+            'entity'      => $entity,
+        );
+    }
+}
