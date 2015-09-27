94977fa75d1fd6bd0a78a9b36728433bb763f906
Jean-Guilhem Rouel <jean-gui@w3.org>
2015-09-27 14:57:22 +0200

    Fixed a bug with subclass feats

--- src/Troulite/PathfinderBundle/Controller/LevelController.php
+++ src/Troulite/PathfinderBundle/Controller/LevelController.php
@@ -76,6 +76,29 @@ class LevelController extends Controller
                     $level->addClassPower((new CharacterClassPower())->setClassPower($power));
                 }
             }
+
+            foreach ($character->getLevels() as $lowerLevel) {
+                if (
+                    $lowerLevel->getClassDefinition() === $level->getClassDefinition()
+                    && $lowerLevel->getSubClasses()->count() > 0
+                ) {
+                    $levelValue = $character->getLevel($level->getClassDefinition());
+                    foreach ($lowerLevel->getSubClasses() as $subClass) {
+                        $alreadyAdded = false;
+                        foreach ($subClass->getPowers($levelValue) as $power) {
+                            foreach ($level->getClassPowers() as $classPower) {
+                                if ($classPower->getClassPower() === $power) {
+                                    $alreadyAdded = true;
+                                    break;
+                                }
+                            }
+                            if (!$alreadyAdded) {
+                                $level->addClassPower((new CharacterClassPower())->setClassPower($power));
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         // Cleanup empty feats that may have been added by the form
