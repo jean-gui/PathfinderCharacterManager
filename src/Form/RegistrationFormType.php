4588598a1c50b52c78a4a7c3c1c58d081fbbc75c
Jean-Guilhem Rouel <jean-gui@w3.org>
2020-09-21 19:30:19 +0200

    improve registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,37 +19,43 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            ->add('name')
+            ->add(
+                'plainPassword',
+                RepeatedType::class,
+                [
+                    'type'            => PasswordType::class,
+                    'invalid_message' => 'The password fields must match.',
+                    'options'         => ['attr' => ['class' => 'password-field']],
+                    'required'        => true,
+                    'first_options'   => ['label' => 'password'],
+                    'second_options'  => ['label' => 'repeat.password'],
+                    'mapped'          => false,
+                    'constraints'     => [
+                        new NotBlank(
+                            [
+                                'message' => 'Please enter a password',
+                            ]
+                        ),
+                        new Length(
+                            [
+                                'min'        => 6,
+                                'minMessage' => 'Your password should be at least {{ limit }} characters',
+                                // max length allowed by Symfony for security reasons
+                                'max'        => 4096,
+                            ]
+                        ),
+                    ],
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => User::class,
+            ]
+        );
     }
 }
