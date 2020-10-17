84b43b2dc21117ac4f75b74a0ce78f29cbaf4ac2
Jean-Guilhem Rouel <jean-gui@w3.org>
2020-10-17 16:46:19 +0200

    Add missing fields to shield admin

--- src/Controller/Admin/Items/ShieldCrudController.php
+++ src/Controller/Admin/Items/ShieldCrudController.php
@@ -4,12 +4,26 @@ namespace App\Controller\Admin\Items;
 
 use App\Entity\Items\Shield;
 use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class ShieldCrudController extends ItemCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Shield::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return array_merge(
+            parent::configureFields($pageName),
+            [
+                IntegerField::new('ac'),
+                IntegerField::new('maximumDexterityBonus'),
+                IntegerField::new('armorCheckPenalty'),
+                IntegerField::new('arcaneSpellFailure')
+            ]
+        );
     }
 
     protected function addClassRestrictions(QueryBuilder $qb): QueryBuilder
