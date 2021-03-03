46511810752ad89032820b2b334239395b528501
Jean-Guilhem Rouel <jean-gui@w3.org>
2021-03-03 18:13:48 +0100

    remove unnecessary comment

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -145,7 +145,6 @@ class User implements UserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 
     public function getName(): ?string
